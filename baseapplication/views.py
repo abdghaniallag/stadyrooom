e672b70baba6546ea3fa74aaa456f60ea9a5dfd8
abdghanialla <abdghaniallag@gmail.com>
2022-07-24 00:01:46 +0100

    adding renders for home and room pages

--- baseapplication/views.py
+++ baseapplication/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render  
-
+from django.shortcuts import render
+from .models import Room
+ 
 def getHome(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
-def getRoom(request):
-    return render(request,'room.html')
+
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'room.html', context)
